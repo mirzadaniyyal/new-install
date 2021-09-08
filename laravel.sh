97b49fcea060ca4807d61225c1265246034377e4
Abdul Basit <basit.engg@gmail.com>
2021-09-09 04:05:21 +0500

    Update laravel.sh

--- laravel.sh
+++ laravel.sh
@@ -6,29 +6,32 @@ export APPFQDN=$APPNAME.com
 apt update
 apt upgrade -y
 
-apt install apache2
+apt install -y apache2 screen tree curl
 systemctl start apache2
-apt install libapache2-mod-php php php-common php-xml php-gd php-opcache php-mbstring php-tokenizer php-json php-bcmath php-zip unzip
-cd /etc/php/7.4/apache2
-nano php.ini
-cd ~
+apt install -y libapache2-mod-php php php-common php-xml php-gd php-opcache php-mbstring php-tokenizer php-json php-bcmath php-zip unzip
+
+PHPVERSION=`php -v | grep "PHP 7" | awk '{print substr($2,0,4)}'`
+
+sed -i 's/cgi.fix_pathinfo=0/cgi.fix_pathinfo=1/g' /etc/php/$PHPVERSION/apache2/php.ini
+
 systemctl restart apache2
-apt install curl
+
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
-composer -V
 composer global require laravel/installer
 
 echo "export PATH='$HOME/.config/composer/vendor/bin:$PATH'" >> ~/.bashrc
 
 source ~/.bashrc
 
-laravel new /home/techblog/$APPNAME
-chgrp -R www-data /home/techblog/$APPNAME
-chmod -R 775 /home/techblog/$APPNAME/storage
-cd /etc/apache2/sites-available/
+cd /home/techblog
 
-cat << EOF > $APPFQDN.conf
+laravel new $APPNAME
+
+chgrp -R www-data /home/techblog/$APPNAME/storage /home/techblog/$APPNAME/bootstrap/cache
+chmod -R ug+rwx /home/techblog/$APPNAME/storage /home/techblog/$APPNAME/bootstrap/cache
+
+cat << EOF > /etc/apache2/sites-available/$APPFQDN.conf
 <VirtualHost *:80>
     ServerName $APPFQDN.com
     ServerAdmin admin@$APPFQDN
